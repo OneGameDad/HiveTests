69575efb01aa110e250e95f38229ec4a7f0de0c1
Gregory Pellechi <gpellech@c1r5p21.hive.fi>
2024-11-15 07:03:54 +0200

    Created a header file with color codes to start building test framework

---
+++ basic.h
@@ -0,0 +1,16 @@
+#ifndefine BASIC_H
+#define BASIC_H
+
+#include <stdio.h>
+
+/* Color definitions for Terminal */
+#define RED "\x1b[31m"
+#define GRN "\x1b[32m"
+#define YEL "\x1b[33m"
+#define BLU "\x1b[34m"
+#define MAG "\x1b[35m"
+#define CYN "\x1b[36m"
+#define WHT "\x1b[37m"
+#define RESET "\x1b[0m"
+
+#endif
